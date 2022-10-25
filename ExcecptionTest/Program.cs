687b375c54f4f19b206e60de70bde82f02d46c29
JoaoFonseca197 <joao.hc.fonseca197@gmail.com>
2022-10-25 10:33:30 +0100

    Exer2

---
+++ ExcecptionTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExcecptionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserira um número interiro:");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"O número inserido{i}");
+        }
+    }
+}
